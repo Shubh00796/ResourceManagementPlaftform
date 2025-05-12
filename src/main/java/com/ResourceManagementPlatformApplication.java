c983dcb1dcd7c230266eb847bfb5d656a7f87671
Shubh00796 <shubhamdongare9696@gmail.com>
2025-05-12 15:21:28 +0530

    Initial commit

---
+++ src/main/java/com/ResourceManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceManagementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceManagementPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ResourceManagementPlatform/ResourceManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ResourceManagementPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceManagementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
